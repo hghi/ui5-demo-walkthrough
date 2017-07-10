bb3092f53606b01f369b12104a0b35efd316a439
Han Ghi <han.ghi.dev@gmail.com>
2017-07-10 20:34:32 +1000

    Step 16: Dialogs and Fragments

--- webapp/controller/hello_panel.controller.js
+++ webapp/controller/hello_panel.controller.js
@@ -5,6 +5,15 @@ sap.ui.define([
    'use strict';
 
    return Controller.extend('sap.ui.demo.wt.controller.hello_panel', {
+       onOpenDialog: function() {
+         var oView = this.getView();
+         var oDialog = oView.byId('helloDialog');
+         if (!oDialog) {
+             oDialog = sap.ui.xmlfragment(oView.getId(), 'sap.ui.demo.wt.view.hello_dialog');
+             oView.addDependent(oDialog);
+         }
+         oDialog.open();
+       },
        onShowHello: function() {
            var oBundle = this.getView().getModel('i18n').getResourceBundle();
            var sRecipient = this.getView().getModel().getProperty('/recipient/name');
