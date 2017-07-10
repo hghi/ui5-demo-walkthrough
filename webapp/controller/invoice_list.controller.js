5fdddbdb75d533c9ae7941c67b5ad55df9ba66bd
Han Ghi <han.ghi.dev@gmail.com>
2017-07-11 03:07:55 +1000

    Step 24: Filtering

--- webapp/controller/invoice_list.controller.js
+++ webapp/controller/invoice_list.controller.js
@@ -1,13 +1,26 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
-    'sap/ui/demo/wt/model/formatter'
-], function(Controller, JSONModel, formatter) {
+    'sap/ui/demo/wt/model/formatter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator'
+], function(Controller, JSONModel, formatter, Filter, FilterOperator) {
     'use strict';
 
     return Controller.extend('sap.ui.demo.wt.controller.invoice_list', {
 
         formatter: formatter,
+
+        onFilterInvoices: function (oEvent) {
+            var aFilter = [];
+            var sQuery = oEvent.getParameter('query');
+            if (sQuery) {
+                aFilter.push(new Filter('ProductName', FilterOperator.Contains, sQuery));
+            }
+            var oList = this.getView().byId('invoiceList');
+            var oBinding = oList.getBinding('items');
+            oBinding.filter(aFilter);
+        },
 
         onInit: function() {
             var oModel = new JSONModel({
