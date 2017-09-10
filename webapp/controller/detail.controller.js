2e99aa2b1a0eba02f598a8bbe7df8e46661836e0
Han Ghi <han.ghi.dev@gmail.com>
2017-09-10 17:10:28 +1000

    Step 34: Custom Controls

--- webapp/controller/detail.controller.js
+++ webapp/controller/detail.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
-    'sap/ui/core/Routing/History'
-], function (Controller, History) {
+    'sap/ui/core/Routing/History',
+    'sap/m/MessageToast'
+], function (Controller, History, MessageToast) {
     'use strict';
 
     return Controller.extend('sap.ui.demo.wt.controller.detail', {
@@ -26,8 +27,14 @@ sap.ui.define([
                 window.history.go(-1);
             } else {
                 var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                oRouter.navTo("overview", {}, true);
+                oRouter.navTo('overview', {}, true);
             }
+        },
+        
+        onRatingChange : function (oEvent) {
+            var fValue = oEvent.getParameter('value');
+            var oResourceBundle = this.getView().getModel('i18n').getResourceBundle();
+            MessageToast.show(oResourceBundle.getText('ratingConfirmation', [fValue]));
         }
     });
 });
